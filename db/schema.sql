38e9e4d1f0cfc76ef548ecf53df3a7eb74e73760
Sam Laxton <samlaxton@Sams-MacBook-Air-2.local>
2022-12-12 19:04:25 +0000

    added foreign keys to table

--- db/schema.sql
+++ db/schema.sql
@@ -9,9 +9,23 @@ CREATE TABLE departments (
     name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT NOT NULL PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
-    salary VARCHAR(100) NOT NULL,
-    department_id INT NOT NULL
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id),
+    FOREIGN KEY (role_id)
+    REFERENCES roles (id)
 );
