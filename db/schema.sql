c83185238210350d77c7cd2d62b7832785415990
Sam Laxton <samlaxton@Sams-MacBook-Air-2.local>
2022-12-12 18:35:04 +0000

    added new table to schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,10 @@ CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE role (
+    id INT NOT NULL PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    salary VARCHAR(100) NOT NULL,
+    department_id INT NOT NULL
+);
