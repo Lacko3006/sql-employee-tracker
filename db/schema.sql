863f734750e8ddc0bc381afa6ca316a69d877e5c
Sam Laxton <samlaxton@Sams-MacBook-Air-2.local>
2022-12-12 18:20:32 +0000

    added departments table to schema.sql file

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS employeetracker_db;
+
+CREATE DATABASE employeetracker_db;
+
+USE employeetracker_db;
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
